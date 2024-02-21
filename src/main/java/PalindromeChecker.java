fdc1f6746348b89773dbc7dca9b5455cd57f4056
eo130 <85082832+eo130@users.noreply.github.com>
2024-02-21 12:51:50 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,13 +37,17 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  //your code here
+  sWord = word.toLowerCase();
+  if(sWord.equals(reverse(sWord)))
+  return true;
+  else
   return false;
 }
 public String reverse(String str)
 {
     String sNew = new String();
-    //your code here
+    for(int i = str.length(); i>0; i--){
+      sNew = sNew + str.substring(i-1,i);
+    }
     return sNew;
-}
 }
